0a7462e74d9fc3a694e94566877e7aff179c410a
Gooooal <gooooal@GooooaldeiMac.local>
2018-10-15 14:08:03 +0800

    add root v c

--- GoalEvents/AppDelegate.swift
+++ GoalEvents/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+
+        let rootVC = RootViewController()
+        let navC = UINavigationController.init(rootViewController: rootVC)
+        self.window?.rootViewController = navC
+
+        
         return true
     }
 
---
+++ GoalEvents/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  GoalEvents
+//
+//  Created by Gooooal on 2018/10/15.
+//  Copyright © 2018年 Gooooal. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        self.navigationItem.title = "Goooal"
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
