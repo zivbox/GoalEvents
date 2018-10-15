7de913d2ac77028ea68f9777a3717f9487b44c69
Gooooal <gooooal@GooooaldeiMac.local>
2018-10-15 13:57:18 +0800

    init ..

---
+++ GoalEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalEvents
+//
+//  Created by Gooooal on 2018/10/15.
+//  Copyright © 2018年 Gooooal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
